407571bf7d7c7ee8461ef05bed64c6378e14adab
Ayla Pearson <ayla@poissonconsulting.ca>
2024-03-18 11:07:41 -0700

    update to newest version of code

--- R/km-survival.R
+++ R/km-survival.R
@@ -52,88 +52,69 @@
 #' survival_est <- km_survival(bboudata::bbousurv_a, "Total", variance = "Greenwood")
 km_survival <- function(x, MortType = "Total", variance = "Pollock") {
   
-  # make sure data set is sorted properly
-  x <- dplyr::arrange(x, .data$PopulationName, .data$Year, .data$Month)
-  # Tally total mortalities.
-  x$TotalMorts <- x$MortalitiesCertain + x$MortalitiesUncertain
-
-  # MortType can be "Total" or "Certain"
-  x$MortType <- MortType
-  x$Morts <- ifelse(x$MortType == "Total", x$TotalMorts, x$MortalitiesCertain)
-
-  # Months with 0 collars monitored are removed but this is noted to user later and estimates scaled appropriately
-  x <- subset(x, x$StartTotal > 0)
-
-  # calculate monthly components of survival and variance
-  LiveDeadCount <- dplyr::mutate(
-    x,
-    Smonth = (1 - (.data$Morts / .data$StartTotal)),
-    Smonth_varj = .data$Morts / (.data$StartTotal * (.data$StartTotal - .data$Morts))
-  )
-
-  # use ddply sum or product each year.
-  YearSurv <- plyr::ddply(
-    LiveDeadCount, 
-    c("PopulationName", "Year"), 
-    plyr::summarize,
-    S = prod(.data$Smonth),
-    S_var1 = sum(.data$Smonth_varj),
-    Survmean = mean(.data$Smonth),
-    sumalive = sum(.data$StartTotal),
-    sumdead = sum(.data$Morts),
-    meanalive = mean(.data$StartTotal),
-    minalive = min(.data$StartTotal),
-    maxalive = max(.data$StartTotal),
-    monthcount = length(.data$Year)
-  )
-
-  YearSurv$VarType <- variance
-
-  # Variance estimate using the Greenwood formula for variance
-  YearSurv$S_Var_Green <- YearSurv$S^2 * YearSurv$S_var1
-  # Variance estimate using the Pollock et al 1989 method
-  YearSurv$S_Var_Pollock <- (YearSurv$S^2 * (1 - YearSurv$S)) / YearSurv$sumalive
-  YearSurv$S_Var <- ifelse(
-    YearSurv$VarType == "Pollock", 
-    YearSurv$S_Var_Pollock, 
-    YearSurv$S_Var_Green
-  )
-
-  # Put note in output if there are no mortalities or less than 12 years.  Zero mortalities causes variance to be 0
-  YearSurv$Status1 <- ifelse(
-    YearSurv$monthcount == 12, 
-    "", 
-    paste("Only", YearSurv$monthcount, "months monitored")
-  )
-  YearSurv$Status2 <- ifelse(
-    YearSurv$sumdead == 0, 
-    "No Mortalities all year (SE=0)", 
-    ""
-  )
-  YearSurv$Status <- paste(YearSurv$Status1, "-", YearSurv$Status2)
-
-  # scale estimates to a year if less than 12 months monitored
-  YearSurv$S <- YearSurv$S^(12 / YearSurv$monthcount)
-  YearSurv$S_Var <- YearSurv$S_Var^(12 / YearSurv$monthcount)
-  YearSurv$S_SE <- YearSurv$S_Var^0.5
-
-  # logit-based confidence intervals--formulas based on program MARK.
-  YearSurv <- dplyr::mutate(
-    YearSurv,
-    logits = log(.data$S / (1 - .data$S)),
-    varlogit = .data$S_Var / (.data$S^2 * ((1 - .data$S)^2))
-  )
-  YearSurv$S_CIU <- 1 / (1 + exp(-1 * (YearSurv$logits + 1.96 * (YearSurv$varlogit**0.5))))
-  YearSurv$S_CIL <- 1 / (1 + exp(-1 * (YearSurv$logits - 1.96 * (YearSurv$varlogit**0.5))))
-
-  # round estimates for table.
-  YearSurv$MeanMonitored <- round(YearSurv$meanalive, 1)
-  YearSurv$S <- round(YearSurv$S, 3)
-  YearSurv$S_SE <- round(YearSurv$S_SE, 3)
-  YearSurv$S_CIL <- round(YearSurv$S_CIL, 3)
-  YearSurv$S_CIU <- round(YearSurv$S_CIU, 3)
-
-  YearSurvR <- YearSurv[c("PopulationName", "Year", "S", "S_SE", "S_CIL", "S_CIU", "MeanMonitored", "sumdead", "sumalive", "Status")]
+  #make sure data set is sorted properly
+  Ssamps<-arrange(Ssamps,PopulationName,Year,Month)
+  #Tally total mortalities.
+  Ssamps$TotalMorts<-Ssamps$MortalitiesCertain+Ssamps$MortalitiesUncertain
+  
+  #MortType can be "Total" or "Certain"
+  Ssamps$MortType<-MortType
+  Ssamps$Morts<-ifelse(Ssamps$MortType=="Total",Ssamps$TotalMorts,Ssamps$MortalitiesCertain)
+  
+  #Months with 0 collars monitored are removed but this is noted to user later and estimates scaled appropriately
+  Ssamps<-subset(Ssamps,Ssamps$StartTotal>0)
+  
+  #calculate monthly components of survival and variance
+  LiveDeadCount<-transform(Ssamps,
+                           Smonth=(1-(Morts/StartTotal)), 
+                           Smonth_varj=Morts/(StartTotal*(StartTotal-Morts)))
+  
+  #use ddply sum or product each year.
+  YearSurv<-ddply(LiveDeadCount, c("PopulationName","Year"),summarize,
+                  S=prod(Smonth),
+                  S_var1=sum(Smonth_varj),
+                  Survmean=mean(Smonth),
+                  sumalive=sum(StartTotal),
+                  sumdead=sum(Morts),
+                  meanalive=mean(StartTotal),
+                  minalive=min(StartTotal),
+                  maxalive=max(StartTotal),
+                  monthcount=length(Year))
+  
+  
+  YearSurv$VarType<-variance
+  
+  #Variance estimate using the Greenwood formula for variance
+  YearSurv$S_Var_Green<- YearSurv$S^2*YearSurv$S_var1 
+  #Variance estimate using the Pollock et al 1989 method
+  YearSurv$S_Var_Pollock<- (YearSurv$S^2*(1-YearSurv$S))/YearSurv$sumalive 
+  YearSurv$S_Var<-ifelse(YearSurv$VarType=="Pollock",YearSurv$S_Var_Pollock,YearSurv$S_Var_Green)
+  
+  #Put note in output if there are no mortalities or less than 12 years.  Zero mortalities causes variance to be 0
+  YearSurv$Status1<-ifelse(YearSurv$monthcount==12,"",paste("Only", YearSurv$monthcount, "months monitored"))
+  YearSurv$Status2<-ifelse(YearSurv$sumdead==0,"No Mortalities all year (SE=0)","")
+  YearSurv$Status<-paste(YearSurv$Status1,"-", YearSurv$Status2)
+  
+  #scale estimates to a year if less than 12 months monitored
+  YearSurv$S<- YearSurv$S^(12/YearSurv$monthcount)
+  YearSurv$S_Var<- YearSurv$S_Var^(12/YearSurv$monthcount)
+  YearSurv$S_SE<-YearSurv$S_Var^0.5
+  
+  #logit-based confidence intervals--formulas based on program MARK.
+  YearSurv<-transform(YearSurv,
+                      logits=log(S/(1-S)), 
+                      varlogit=S_Var/(S^2*((1-S)^2)))
+  YearSurv$S_CIU<-1/(1+exp(-1*(YearSurv$logits+ 1.96*(YearSurv$varlogit**0.5)))) 
+  YearSurv$S_CIL<-1/(1+exp(-1*(YearSurv$logits- 1.96*(YearSurv$varlogit**0.5)))) 
+  
+  #round estimates for table.
+  YearSurv$MeanMonitored<-round(YearSurv$meanalive,1)
+  YearSurv$S<-round(YearSurv$S,3)
+  YearSurv$S_SE<-round(YearSurv$S_SE,3)
+  YearSurv$S_CIL<-round(YearSurv$S_CIL,3)
+  YearSurv$S_CIU<-round(YearSurv$S_CIU,3)
+  
+  YearSurvR<-YearSurv[c("PopulationName",'Year','S','S_SE','S_CIL','S_CIU','MeanMonitored',"sumdead","sumalive","Status")]
   
   YearSurvR
 }
--- R/recruitment.R
+++ R/recruitment.R
@@ -65,76 +65,78 @@
 #'  variance <- "binomial"
 #' )
 recruitment <- function(x, pFemales, sexratio, variance) {
-  # Estimate total females based on pFemales and sexratio
-  x <- dplyr::mutate(
-    x,
-    Females = .data$Cows + .data$UnknownAdults * pFemales + .data$Yearlings * sexratio,
-    FemaleCalves = .data$Calves * sexratio
-  )
-
-  # summarize by population and year
-  Compfull <-plyr::ddply(
-    x,
-    c("PopulationName", "Year"),
-    plyr::summarize,
-    Females = sum(.data$Females),
-    FemaleCalves = sum(.data$FemaleCalves),
-    Calves = sum(.data$Calves),
-    UnknownAdults = sum(.data$UnknownAdults),
-    Bulls = sum(.data$Bulls),
-    Yearlings = sum(.data$Yearlings),
-    groups = length(.data$Year)
-  )
-
-  # Estimate recruitment based on full data set.
-  # Calf cow based on male/female calves
-  Compfull$CalfCow <- Compfull$Calves / Compfull$Females
-  # Calf cow for female calves is 1/2 of CC with female/male calves
-  Compfull$CalfCowF <- Compfull$CalfCow * sexratio
-  # Recruitment using DeCesare correction
-  Compfull$R <- Compfull$CalfCowF / (1 + Compfull$CalfCowF)
-
-  # variance estimation-in progress.....
-  # simple binomial variance estimate-right now uses females but may not be statistically correct!
-  if (variance == "binomial") {
-    Compfull$BinVar <- (Compfull$R * (1 - Compfull$R)) / Compfull$Females
-    Compfull$R_SE <- Compfull$BinVar^0.5
-
-    # logit-based confidence limits assuing R is constrained between 0 and 1.
-    Compfull <- dplyr::mutate(
-      Compfull,
-      logits = log(.data$R / (1 - .data$R)),
-      varlogit = .data$BinVar / (.data$R^2 * ((1 - .data$R)^2))
-    )
-    Compfull$R_CIU <- 1 / (1 + exp(-1 * (Compfull$logits + 1.96 * (Compfull$varlogit**0.5))))
-    Compfull$R_CIL <- 1 / (1 + exp(-1 * (Compfull$logits - 1.96 * (Compfull$varlogit**0.5))))
+  #Estimate total females based on pFemales and sexratio
+  x<-transform(x,
+                    Females=Cows+UnknownAdults*pFemales+Yearlings*sexratio,
+                    FemaleCalves=Calves*sexratio)
+  
+  #summarize by population and year
+  Compfull<-ddply(x,c("PopulationName","Year"),summarize,
+                  Females=sum(Females),
+                  FemaleCalves=sum(FemaleCalves),
+                  Calves=sum(Calves),
+                  UnknownAdults=sum(UnknownAdults),
+                  Bulls=sum(Bulls),
+                  Yearlings=sum(Yearlings),
+                  groups=length(Year))
+  
+  #Estimate recruitment based on full data set.
+  #Calf cow based on male/female calves           
+  Compfull$CalfCow<-Compfull$Calves/Compfull$Females
+  #Calf cow for female calves is 1/2 of CC with female/male calves
+  Compfull$CalfCowF<-Compfull$CalfCow*sexratio
+  #Recruitment using DeCesare correction
+  Compfull$R<-Compfull$CalfCowF/(1+Compfull$CalfCowF)
+  
+  #variance estimation-in progress.....
+  #simple binomial variance estimate-right now uses females but may not be statistically correct!
+  if (variance=="binomial") {
+    Compfull$BinVar<-(Compfull$R*(1-Compfull$R))/Compfull$Females
+    Compfull$R_SE<-Compfull$BinVar^0.5
+    
+    #logit-based confidence limits assuing R is constrained between 0 and 1.
+    Compfull<-transform(Compfull,
+                        logits=log(R/(1-R)), 
+                        varlogit=BinVar/(R^2*((1-R)^2)))
+    Compfull$R_CIU<-1/(1+exp(-1*(Compfull$logits+ 1.96*(Compfull$varlogit**0.5))))
+    Compfull$R_CIL<-1/(1+exp(-1*(Compfull$logits- 1.96*(Compfull$varlogit**0.5))))
   }
-
-  # bootstrap approach...in progress....
-  if (variance == "bootstrap") {
-
-    # use ddply to bootstrap by Population and year
-    boot <- plyr::dlply(
-      x, 
-      c("PopulationName", "Year"), 
-      function(x) boot(data = x, RecCalc, R = 1000)
-    )
-    # need to write a way to extract SE and percentile CI's from boot list.....
+  
+  #bootstrap approach...in progress....
+  if (variance=="bootstrap") {
+    
+    #a function to bootrap
+    RecCalc<-function(C,indices) {
+      d <- C[indices,]
+      CCF<-sum(d$FemaleCalves)/sum(d$Females)
+      Rec<-CCF/(1+CCF)
+      return(Rec)
+    }
+    
+    #use ddply to bootstrap by Population and year
+    boot<-  dlply(x,c("PopulationName","Year"), function(x) boot(data=x, RecCalc,R=1000))
+    
+    pc <- purrr::map_df(names(boot), function(x){
+      boots <- boot[[x]]$t
+      year <- strsplit(x, split = "\\.")[[1]][2]
+      pop <- strsplit(x, split = "\\.")[[1]][1]
+      quants <- quantile(boots, c(0.025, 0.5, 0.975))
+      SE<-sd(boots)
+      tibble::tibble(PopulationName = pop, 
+                     Year = year, 
+                     medianboot = quants[2],
+                     R_SE=SE,
+                     R_CIL = quants[1],
+                     R_CIU = quants[3])
+    })
+    
+    Compfull<-merge(Compfull,pc[c("PopulationName","Year","R_SE","R_CIL","R_CIU")],by=c("PopulationName","Year"))
   }
-
-  # add in other formula with finite correction??  To do this would need data frame of f values......
-
-  # An abbreviated output data set.
-  CompfullR <- cbind(Compfull[c("PopulationName", "Year", "R", "R_SE", "R_CIL", "R_CIU", "groups", "FemaleCalves", "Females")], sexratio, pFemales)
-  CompfullR[c(3:6)] <- round(CompfullR[c(3:6)], 3)
-
-  CompfullR
-}
-
-# a function to bootrap
-RecCalc <- function(C, indices) {
-  d <- C[indices, ]
-  CCF <- sum(d$FemaleCalves) / sum(d$Females)
-  Rec <- CCF / (1 + CCF)
-  return(Rec)
+  
+  
+  #An abbreviated output data set.
+  CompfullR<-cbind(Compfull[c("PopulationName","Year","R","R_SE", "R_CIL","R_CIU","groups","FemaleCalves","Females")], sexratio , pFemales)
+  CompfullR[c(3:6)]<-round(CompfullR[c(3:6)],3)
+  
+  return (CompfullR)
 }
