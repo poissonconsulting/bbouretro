0ada7060a39012f10ce875da4d743446028b0293
Ayla Pearson <ayla@poissonconsulting.ca>
2024-04-16 14:31:32 -0700

    update documentation to be consistent

--- R/plot-growth-distributions.R
+++ R/plot-growth-distributions.R
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Plot lambda distributions
+#' Plot population growth distributions
 #'
 #' Create histograms of simulated population growth (\eqn{\lambda}) values for a
 #' chosen population unit.
